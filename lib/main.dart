b5d58f15c759106d33efe9766e4e5c757308ef9f
Jeha <yelsum0209@sookmyung.ac.kr>
2024-11-21 17:19:04 +0900

    불필요한 주석 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:halmoney/FirestoreData/JobProvider.dart';
-import 'package:halmoney/get_user_info/step1_welcome.dart';
 import 'package:halmoney/pages/login_page.dart';
-import 'package:halmoney/AI_pages/search_engine.dart';
-import 'package:halmoney/screens/myPage/myPage.dart';
-import 'package:halmoney/screens/resume/step1_hello.dart';
-import 'package:halmoney/screens/home/home.dart';
-import 'myAppPage.dart';
-import 'package:halmoney/signup_pages/signupStepper_page1.dart';
 import 'package:halmoney/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -35,9 +28,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  //final String id ='sumin1234';
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,18 +41,11 @@ class MyApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
-        //Locale('en', ''), //English, no country code
         Locale('ko', ''), //Korean, no country code
       ],
       debugShowCheckedModeBanner:  false,
 
-
       home: LoginPage(),
-      // 아래는 테스트용으로 원하는 페이지 연결용
-      // git에 올릴때 반드시 수정
-      // home: StepWelcome(id: "silver1234"),
-      // home: MyHomePage(id:'ralral')
-
     );
   }
 }
