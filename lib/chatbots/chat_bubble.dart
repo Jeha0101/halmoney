44c806efd5d5ca4d47dffd7f6339f3a80836ea00
Jeha <yelsum0209@sookmyung.ac.kr>
2024-11-25 20:56:18 +0900

    Create chat_bubble.dart
    채팅 버블 위젯 생성

---
+++ lib/chatbots/chat_bubble.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  final String messageContent;
+  final bool isUserMessage;
+  Alignment messageLocation = Alignment.centerLeft;
+  Color messageBoxColor = Colors.grey;
+  Color messageTextColor = Colors.black;
+
+  ChatBubble({super.key, required this.messageContent, required this.isUserMessage}){
+    if (isUserMessage) {
+      messageLocation = Alignment.centerRight;
+      messageBoxColor = Color.fromARGB(250, 51, 51, 255);
+      messageTextColor = Colors.white;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: messageLocation,
+        child: Container(
+            margin: const EdgeInsets.all(5.0),
+            padding: const EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              color: messageBoxColor,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Text(
+              messageContent,
+              style: TextStyle(
+                color: messageTextColor,
+                fontSize: 16.0,
+              ),
+            )));
+  }
+}
