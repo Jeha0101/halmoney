e12af44807d64c6d54c1a8081e69f57682c0ad1b
Jeha <yelsum0209@sookmyung.ac.kr>
2024-11-28 02:35:46 +0900

    Create standard_app_bar.dart

---
+++ lib/standard_app_bar.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class StandardAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String? title;
+  final String leftText;
+  final String rightText;
+  final VoidCallback? onBackPressed;
+  final VoidCallback? onNextPressed;
+
+  const StandardAppBar({
+    Key? key,
+    this.title,
+    this.leftText = "이전",
+    this.rightText = "다음",
+    this.onBackPressed,
+    this.onNextPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: const Color.fromARGB(250, 51, 51, 255),
+      elevation: 1.0,
+      automaticallyImplyLeading: false,
+      title: Row(
+        children: [
+          if (onBackPressed != null)
+            GestureDetector(
+              onTap: onBackPressed,
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.chevron_left,
+                    size: 30,
+                  ),
+                  Text(
+                    leftText,
+                    style: const TextStyle(
+                      fontFamily: 'NanumGothicFamily',
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          else
+            const SizedBox(width: 60),
+
+          if (title != null)
+            Expanded(
+              child: Center(
+                child: Text(
+                  title!,
+                  style: const TextStyle(
+                    fontFamily: 'NanumGothicFamily',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          else
+            const Spacer(),
+
+          if (onNextPressed != null)
+            GestureDetector(
+              onTap: onNextPressed,
+              child: Row(
+                children: [
+                  Text(
+                    rightText,
+                    style: const TextStyle(
+                      fontFamily: 'NanumGothicFamily',
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Icon(
+                    Icons.chevron_right,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            )
+          else
+            const SizedBox(width: 60),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
